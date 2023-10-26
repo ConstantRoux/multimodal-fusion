463f73841b228ed75e89461a11da38fd92e4ccf9
ConstantRoux <constant.roux31@gmail.com>
2023-10-26 11:41:12 +0200

    Add slot array

---
+++ SlotData.py
@@ -0,0 +1,30 @@
+from enum import Enum
+
+
+dataOrd = 5
+
+
+class DataType(Enum):
+    ACTION = 0
+    SHAPE = 1
+    COLOR = 2
+    POSITION_1 = 3
+    POSITION_2 = 4
+
+
+class ActionType(Enum):
+    ADD = 0
+    MOVE = 1
+    DELETE = 2
+
+
+class ShapeType(Enum):
+    SQUARE = 0
+    CIRCLE = 1
+    TRIANGLE = 2
+
+
+class ColorType(Enum):
+    RED = 0
+    GREEN = 1
+    BLUE = 2
